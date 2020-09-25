44250f298d8340f69a9341e760b6954841cab50a
Alain Pereida Prado <alain.pereida@uabc.edu.mx>
2020-09-24 17:33:26 -0700

    Se agrego el diseño del login y tambien los componentes que se usaron.

--- app/src/main/java/com/example/pokeapi/LoginActivity.java
+++ app/src/main/java/com/example/pokeapi/LoginActivity.java
@@ -1,9 +1,14 @@
 package com.example.pokeapi;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+//import me.sargunvohra.lib.pokekotlin.client.PokeApi;
+//import me.sargunvohra.lib.pokekotlin.client.PokeApiClient;
+//import me.sargunvohra.lib.pokekotlin.model.PokemonSpecies;
+//
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,5 +17,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+//        PokeApi pokeApi = new PokeApiClient();
+//        System.out.println(pokeApi.getPokemon(1));
     }
 }
